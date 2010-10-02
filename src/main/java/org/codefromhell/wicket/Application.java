18f0568efe0395bfbb12e0bce7e009c18c990f71
Marco Grunert <magomi+github@gmail.com>
2010-10-02 11:20:15 +0200

    initial import

---
+++ src/main/java/org/codefromhell/wicket/Application.java
@@ -0,0 +1,42 @@
+package org.codefromhell.wicket;
+
+import org.apache.wicket.Page;
+import org.apache.wicket.protocol.http.HttpSessionStore;
+import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.session.ISessionStore;
+
+/**
+ * Application object for the webapp.
+ *
+ * @author Marco Grunert (magomi@gmail.com)
+ */
+public class Application extends WebApplication
+{    
+	/**
+	 * Constructor.
+	 */
+	public Application() {
+	}
+
+    /**
+     * Returns the class of the page that should be loaded on first access to the application.
+     *
+     * @return The class of the applications start page.
+     */
+	@Override
+	public Class<? extends Page> getHomePage() {
+		return HomePage.class;
+	}
+
+    @Override
+	protected void init() {
+		super.init();
+
+		this.getResourceSettings().setResourcePollFrequency(null);
+	}
+
+    @Override
+	protected ISessionStore newSessionStore() {	
+		return new HttpSessionStore(this);
+	}
+}
